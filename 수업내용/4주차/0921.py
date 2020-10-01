0c9e6fc6b0740f149da057250a353304be2ddbc9
DESKTOP-1RBEQLJ\SoHyun <chsh981127@naver.com>
2020-10-02 05:17:23 +0900

    handle_events()를 handle_event로 변경

--- 수업내용/4주차/0921.py
+++ 수업내용/4주차/0921.py
@@ -3,6 +3,7 @@ from gobj import *
 import os
 os.chdir('d:/SoHyun/문서/2DGP/수업내용/3주차')
 
+# 프로그램마다 달라지는 부분을 이곳에 쓸 것이다.
 def enter():
 	global grass, team
 	grass = Grass()
@@ -16,14 +17,12 @@ def draw():
 	for b in team: b.draw()
 
 # extract to function
-def handle_events():
+def handle_event(e):
 	global running
-	evts = get_events()
-	for e in evts:
-		if e.type == SDL_QUIT:
-			running = False
-		elif (e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
-			running = False
+	if e.type== SDL_QUIT:
+		running= False
+	elif(e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+		running False
 
 open_canvas() 
 
@@ -31,9 +30,11 @@ enter()
 
 running = True
 
+# 여기는 안 바뀔 부분
 while running:
 	# event handling
-	handle_events()
+	evts = get_events()
+	for e in evts: handle_event(e)
 
 	# game logic
 	update() 
@@ -42,7 +43,7 @@ while running:
 	clear_canvas()
 	draw()
 	update_canvas()
-	
+
 	delay (0.01)
 
 close_canvas()
