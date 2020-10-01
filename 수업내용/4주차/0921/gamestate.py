0d38d073b7b98cbb620d30de5d7936ff8c0d9534
DESKTOP-1RBEQLJ\SoHyun <chsh981127@naver.com>
2020-10-02 05:24:05 +0900

    game framework 만들고 이에 맞도록 수정

---
+++ 수업내용/4주차/0921/gamestate.py
@@ -0,0 +1,24 @@
+from pico2d import *
+from gobj import *
+import os
+os.chdir('d:/SoHyun/문서/2DGP/수업내용/3주차')
+
+# 프로그램마다 달라지는 부분을 이곳에 쓸 것이다.
+def enter():
+	global grass, team
+	grass = Grass()
+	team = [ Boy() for i in range(11) ]
+
+def update():
+	for b in team: b.update()
+
+def draw():
+	grass.draw()
+	for b in team: b.draw()
+
+def handle_event(e):
+	global running
+	if e.type== SDL_QUIT:
+		running= False
+	elif(e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+		running False
